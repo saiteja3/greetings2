cd7e1c208b6cfb0ceb4444bcf47c1665c7715bf4
saiteja3 <42633885+saiteja3@users.noreply.github.com>
2019-05-07 12:52:33 +0530

    Create greestings2.java

---
+++ greestings2.java
@@ -0,0 +1,21 @@
+class ample{
+void greestins(){
+System.out.println("hello');
+System.out.println("have a nice day");}
+void sum(int x,int y){
+int s;
+s=x+y;
+System.out.println("sum is :"+s);}
+int sq(int x){
+int s;
+s=x*x;
+return s;}
+public static void main(String args[])
+{
+sample s=new sample();
+s.greetins();
+s.sum(5,7);
+int r=s.sq(7);
+System.out.println("result is :"+r)
+}
+}
